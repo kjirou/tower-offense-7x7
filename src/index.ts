456cce84234d8abbe030fee11a679ba2c46ef9a7
kjirou <sorenariblog@gmail.com>
2019-04-14 15:17:13 +0900

    DNM: Parcel is not suitable for TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import foo from './foo'
-console.log(foo)
+// 型が正しくない
+let a: number = 1;
+a = 'abc';
+
+// const へ再代入
+const b = 1;
+b = 2;
+
+// 構文エラー
+const c: = 1;
+
+console.log('Hello, index.ts!');
