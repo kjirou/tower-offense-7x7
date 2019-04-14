6e7c103b84f581317db21f4256e9a1e4d8802913
kjirou <sorenariblog@gmail.com>
2019-04-14 23:34:37 +0900

    DNM: Is types/styled-components incompatible to types/react?
    ```
    ERROR in /Users/ishii.koujirou/app/tower-offense-7x7/src/components/Root.tsx
    ./src/components/Root.tsx
    [tsl] ERROR in /Users/ishii.koujirou/app/tower-offense-7x7/src/components/Root.tsx(49,10)
          TS2322: Type '{ position: string; width: string; height: string; backgroundColor: string; }' is not assignable to type 'CSSProperties'.
      Types of property 'position' are incompatible.
        Type 'string' is not assignable to type '"relative" | "-moz-initial" | "inherit" | "initial" | "revert" | "unset" | "-webkit-sticky" | "absolute" | "fixed" | "static" | "sticky" | undefined'.
    
    ERROR in /Users/ishii.koujirou/app/tower-offense-7x7/src/components/Root.tsx
    ./src/components/Root.tsx
    [tsl] ERROR in /Users/ishii.koujirou/app/tower-offense-7x7/src/components/Root.tsx(65,10)
          TS2322: Type '{ position: string; margin: string; width: string; height: string; }' is not assignable to type 'CSSProperties'.
      Types of property 'position' are incompatible.
        Type 'string' is not assignable to type '"relative" | "-moz-initial" | "inherit" | "initial" | "revert" | "unset" | "-webkit-sticky" | "absolute" | "fixed" | "static" | "sticky" | undefined'.
    ```

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,34 +1,45 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
-function MetaInformationBar(): JSX.Element {
-  const style = {
-    display: 'relative',
-    width: '360px',
-    height: '48px',
-    backgroundColor: 'yellow',
-  };
+type BattleFieldElement = {
+  x: number,
+  y: number,
+};
 
-  return (
-    <div style={style}>MetaInformationBar!</div>
-  );
+const dummyMatrix: BattleFieldElement[][] = [];
+for (let y = 0; y < 7; y++) {
+  const row: BattleFieldElement[] = [];
+  for (let x = 0; x < 7; x++) {
+    row.push({
+      y,
+      x,
+    });
+  }
+  dummyMatrix.push(row);
 }
 
-function Board(): JSX.Element {
-  const style = {
-    display: 'relative',
-    width: '360px',
-    height: '360px',
-    backgroundColor: 'green',
-  };
+const MetaInformationBar = styled.div`
+  position: relative;
+  width: 360px;
+  height: 48px;
+  background-color: yellow;
+`;
 
-  return (
-    <div style={style}>Board!</div>
-  );
-}
+const Square = styled.div`
+  width: 48px;
+  height: 48px;
+`;
+
+const Board = styled.div`
+  position: relative;
+  width: 360px;
+  height: 360px;
+  background-color: green;
+`;
 
 function BattlePage(): JSX.Element {
   const style = {
-    display: 'relative',
+    position: 'relative',
     width: '360px',
     height: '640px',
     backgroundColor: 'silver',
@@ -36,15 +47,15 @@ function BattlePage(): JSX.Element {
 
   return (
     <div style={style}>
-      <MetaInformationBar />
-      <Board />
+      <MetaInformationBar>MetaInformationBar!!</MetaInformationBar>
+      <Board>Board!!</Board>
     </div>
   );
 }
 
 export default function Root(): JSX.Element {
   const style = {
-    display: 'relative',
+    position: 'relative',
     margin: '0 auto',
     width: '360px',
     height: '640px',
