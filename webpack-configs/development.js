42335df864681c719b76427d619a15cf1810de8d
kjirou <sorenariblog@gmail.com>
2019-04-14 20:33:17 +0900

    Show the first development page

--- webpack-configs/development.js
+++ webpack-configs/development.js
@@ -1,9 +1,11 @@
+const ejs = require('ejs');
 const path = require('path');
 const webpack = require('webpack');
 
 const {
   DIST_ROOT,
   SRC_ROOT,
+  WEBPACK_DEV_SERVER_ROOT,
   sharedWebpackConfig,
 } = require('./shared');
 
@@ -30,4 +32,34 @@ module.exports = {
       __TEST__: false,
     }),
   ],
+  devServer: {
+    publicPath: '/tower-offense-7x7/',
+
+    compress: true,
+
+    port: 9000,
+
+    before(app) {
+      const genereateCommonTemplateVariables = () => {
+        return {
+        };
+      };
+
+      app.engine('html', ejs.renderFile);
+
+      app.get('/', (req, res) => {
+        res.render(
+          path.join(WEBPACK_DEV_SERVER_ROOT, 'templates/index.html'),
+          genereateCommonTemplateVariables()
+        );
+      });
+
+      app.get('/*.html', (req, res) => {
+        res.render(
+          path.join(WEBPACK_DEV_SERVER_ROOT, 'templates', req.path),
+          genereateCommonTemplateVariables()
+        );
+      });
+    },
+  },
 };
--- webpack-configs/shared.js
+++ webpack-configs/shared.js
@@ -3,6 +3,8 @@ const path = require('path');
 const PROJECT_ROOT = path.join(__dirname, '..');
 const SRC_ROOT = path.join(PROJECT_ROOT, 'src');
 const DIST_ROOT = path.join(PROJECT_ROOT, 'dist');
+const WEBPACK_CONFIGS_ROOT = path.join(PROJECT_ROOT, 'webpack-configs');
+const WEBPACK_DEV_SERVER_ROOT = path.join(WEBPACK_CONFIGS_ROOT, 'dev-server');
 
 const sharedWebpackConfig = {
   module: {
@@ -17,7 +19,7 @@ const sharedWebpackConfig = {
     ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx'],
+    extensions: ['.js', '.ts', '.tsx'],
   },
 };
 
@@ -25,5 +27,6 @@ module.exports = {
   DIST_ROOT,
   PROJECT_ROOT,
   SRC_ROOT,
+  WEBPACK_DEV_SERVER_ROOT,
   sharedWebpackConfig,
 };
