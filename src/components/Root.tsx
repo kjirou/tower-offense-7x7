c1a622df3d63cf23b2df343f11019c4f07ebd29d
kjirou <sorenariblog@gmail.com>
2019-04-15 20:42:52 +0900

    Draw 7x7 squares

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 
+type BattleFieldSquareProp = {
+  x: number,
+  y: number,
+};
+
 function MetaInformationBar(): JSX.Element {
   const style = {
     position: 'relative',
@@ -13,7 +18,22 @@ function MetaInformationBar(): JSX.Element {
   );
 }
 
-function Board(): JSX.Element {
+function BattleFieldSquare(props: BattleFieldSquareProp): JSX.Element {
+  const style = {
+    position: 'absolute',
+    top: `${6 + props.y * 48 + props.y * 2}px`,
+    left: `${6 + props.x * 48 + props.x * 2}px`,
+    width: '48px',
+    height: '48px',
+    backgroundColor: 'lime',
+  };
+
+  return (
+    <div style={style}>{`(${props.y},${props.x})`}</div>
+  );
+}
+
+function BattleFieldBoard(props: {board: BattleFieldSquareProp[][]}): JSX.Element {
   const style = {
     position: 'relative',
     width: '360px',
@@ -21,12 +41,39 @@ function Board(): JSX.Element {
     backgroundColor: 'green',
   };
 
+  // TODO: flatten
+  const flattened: BattleFieldSquareProp[] = [];
+  props.board.forEach(row => {
+    row.forEach(square => {
+      flattened.push(square);
+    });
+  });
+
   return (
-    <div style={style}>Board!</div>
+    <div style={style}>
+    {
+      flattened.map((square) => {
+        const key = `square-${square.y}-${square.x}`;
+        return <BattleFieldSquare key={key} y={square.y} x={square.x} />;
+      })
+    }
+    </div>
   );
 }
 
 function BattlePage(): JSX.Element {
+  const boardProps: BattleFieldSquareProp[][] = [];
+  for (let y = 0; y < 7; y++) {
+    const row: BattleFieldSquareProp[] = [];
+    for (let x = 0; x < 7; x++) {
+      row.push({
+        y,
+        x,
+      });
+    }
+    boardProps.push(row);
+  }
+
   const style = {
     position: 'relative',
     width: '360px',
@@ -37,7 +84,7 @@ function BattlePage(): JSX.Element {
   return (
     <div style={style}>
       <MetaInformationBar />
-      <Board />
+      <BattleFieldBoard board={boardProps} />
     </div>
   );
 }
