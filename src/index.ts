6b01f8644c828e0186dd60193172e5b826296628
kjirou <sorenariblog@gmail.com>
2019-04-15 23:13:05 +0900

    Assume the necessary state

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import Root from './components/Root';
+import {createInitialApplicationState} from './state-manager';
 
 window.addEventListener('DOMContentLoaded', function() {
   const appDestination = document.querySelector('.js-app');
 
   if (appDestination) {
+    const applicationState = createInitialApplicationState();
+
     ReactDOM.render(
       React.createElement(Root, {}),
       appDestination
---
+++ src/state-manager/index.ts
@@ -0,0 +1,59 @@
+type FactionId = 'ally' | 'enemy';
+
+type Creature = {
+  attackPoint: number,
+  lifePoint: number,
+  id: string,
+}
+
+type Party = {
+  factionId: FactionId,
+  creatureIds: Creature['id'][],
+}
+
+type BattleFieldElementState = {
+  creatureId: Creature['id'] | void,
+  x: number,
+  y: number,
+}
+
+type GameState = {
+  battleFieldMatrix: BattleFieldElementState[][],
+  creatures: Creature[],
+  parties: Party[],
+}
+
+type PageState = {
+  game?: GameState,
+  pageId: 'welcome' | 'battle',
+}
+
+type ApplicationState = {
+  activatedPage: PageState,
+}
+
+export function createInitialApplicationState(): ApplicationState {
+  const battleFieldMatrix: BattleFieldElementState[][] = [];
+  for (let y = 0; y < 7; y++) {
+    const row: BattleFieldElementState[] = [];
+    for (let x = 0; x < 7; x++) {
+      row.push({
+        y,
+        x,
+        creatureId: undefined,
+      });
+    }
+    battleFieldMatrix.push(row);
+  }
+
+  return {
+    activatedPage: {
+      pageId: 'battle',
+      game: {
+        creatures: [],
+        parties: [],
+        battleFieldMatrix,
+      },
+    },
+  };
+}
