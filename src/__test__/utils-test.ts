8727998212ab5025081f3f57e274fc7012dabcbc
kjirou <sorenariblog@gmail.com>
2019-04-23 20:36:01 +0900

    Make to execute "npm test"

---
+++ src/__test__/utils-test.ts
@@ -0,0 +1,13 @@
+import * as assert from 'assert';
+import {describe, it} from 'mocha';
+
+import {
+  flattenMatrix,
+} from '../utils';
+
+describe('utils', function() {
+  describe('flattenMatrix', function() {
+    it('tmp', function() {
+    });
+  });
+});
