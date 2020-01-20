da067694a43d7b7e9e1a667d14b0f86655526363
kjirou <sorenariblog@gmail.com>
2020-01-20 13:20:36 +0900

    Create a part of the normal attack process

--- src/state-manager/game/battle-process.ts
+++ src/state-manager/game/battle-process.ts
@@ -1,14 +1,30 @@
 import {
+  BattleFieldElement,
   Creature,
   Party,
   NormalAttackContext,
+  determineRelationshipBetweenFactions,
   findCreatureByIdOrError,
+  findBattleFieldElementByCreatureId,
+  findBattleFieldElementsByDistance,
 } from './utils';
 
-function findCreatureWithParty(creatures: Creature[], parties: Party[], creatureId: Creature['id']): {
+type CreatureWithParty = {
   creature: Creature,
   party: Party,
-} {
+}
+
+type CreatureWithPartyOnBattleFieldElement = {
+  creature: Creature,
+  party: Party,
+  battleFieldElement: BattleFieldElement,
+}
+
+function findCreatureWithParty(
+  creatures: Creature[],
+  parties: Party[],
+  creatureId: Creature['id']
+): CreatureWithParty {
   for (let partyIndex = 0; partyIndex < parties.length; partyIndex++) {
     const party = parties[partyIndex]
     for (let creatureIdIndex = 0; creatureIdIndex < party.creatureIds.length; creatureIdIndex++) {
@@ -25,12 +41,39 @@ function findCreatureWithParty(creatures: Creature[], parties: Party[], creature
   throw new Error('Can not find the `creatureId` from `parties`.')
 }
 
-//function isCreatureEnemy() {
-//}
-
 export function invokeNormalAttack(context: NormalAttackContext): NormalAttackContext {
   const attackerWithParty = findCreatureWithParty(context.creatures, context.parties, context.attackerCreatureId)
-  // TODO: 異なる派閥で射程範囲内のクリーチャーを抽出
+
+  // 攻撃者情報を抽出する。
+  const attackerData: CreatureWithPartyOnBattleFieldElement = {
+    creature: attackerWithParty.creature,
+    party: attackerWithParty.party,
+    battleFieldElement: findBattleFieldElementByCreatureId(context.battleFieldMatrix, context.attackerCreatureId),
+  }
+
+  // 攻撃対象者候補である、射程範囲内で敵対関係のクリーチャー情報を抽出する。
+  const targetCandidatesData: CreatureWithPartyOnBattleFieldElement[] = []
+  const dummyDistance = 1
+  const reachableBattleFieldElements = findBattleFieldElementsByDistance(
+    context.battleFieldMatrix, attackerData.battleFieldElement.position, dummyDistance)
+  for (const reachableBattleFieldElement of reachableBattleFieldElements) {
+    if (reachableBattleFieldElement.creatureId !== undefined) {
+      const creatureWithParty =
+        findCreatureWithParty(context.creatures, context.parties, reachableBattleFieldElement.creatureId)
+      if (
+        determineRelationshipBetweenFactions(
+          creatureWithParty.party.factionId, attackerData.party.factionId
+        ) === 'enemy'
+      ) {
+        targetCandidatesData.push({
+          creature: creatureWithParty.creature,
+          party: creatureWithParty.party,
+          battleFieldElement: reachableBattleFieldElement,
+        })
+      }
+    }
+  }
+
   // TODO: ターゲット可能数の算出
   // TODO: 優先順位を考慮したターゲットの決定
   // TODO: ダメージ計算とコンテキストへの反映
