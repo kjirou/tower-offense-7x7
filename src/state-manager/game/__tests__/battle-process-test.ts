fa9df40ca2a3370e9a67a62775272a1d16da521c
kjirou <sorenariblog@gmail.com>
2020-01-20 21:48:02 +0900

    Add test cases

--- src/state-manager/game/__tests__/battle-process-test.ts
+++ src/state-manager/game/__tests__/battle-process-test.ts
@@ -4,10 +4,90 @@ import {describe, it} from 'mocha'
 import {
   invokeNormalAttack,
 } from '../battle-process'
+import {
+  NormalAttackContext,
+  createBattleFieldMatrix,
+  findCreatureByIdOrError,
+} from '../utils'
 
 describe('state-manager/game/battle-process', function() {
   describe('invokeNormalAttack', function() {
-    it('works', function() {
+    describe('An attacker is adjacent to a single enemy', function() {
+      const matrix = createBattleFieldMatrix(3, 3)
+      const attacker = {
+        id: 'a',
+        jobId: '',
+        attackPoint: 1,
+        lifePoint: 1,
+      }
+      const enemy = {
+        id: 'e',
+        jobId: '',
+        attackPoint: 1,
+        lifePoint: 1,
+      }
+      matrix[1][1].creatureId = attacker.id
+      matrix[1][2].creatureId = enemy.id
+      const context: NormalAttackContext = {
+        creatures: [attacker, enemy],
+        parties: [
+          {
+            factionId: 'ally',
+            creatureIds: [attacker.id],
+          },
+          {
+            factionId: 'enemy',
+            creatureIds: [enemy.id],
+          }
+        ],
+        battleFieldMatrix: matrix,
+        attackerCreatureId: attacker.id,
+      }
+
+      it('can attack the enemy', function() {
+        const newContext = invokeNormalAttack(context)
+        const newEnemy = findCreatureByIdOrError(newContext.creatures, enemy.id)
+        assert.strictEqual(newEnemy.lifePoint, 0)
+      })
+    })
+
+    describe('An attacker and its enemy are two distances apart', function() {
+      const matrix = createBattleFieldMatrix(3, 3)
+      const attacker = {
+        id: 'a',
+        jobId: '',
+        attackPoint: 1,
+        lifePoint: 1,
+      }
+      const enemy = {
+        id: 'e',
+        jobId: '',
+        attackPoint: 1,
+        lifePoint: 1,
+      }
+      matrix[1][0].creatureId = attacker.id
+      matrix[1][2].creatureId = enemy.id
+      const context: NormalAttackContext = {
+        creatures: [attacker, enemy],
+        parties: [
+          {
+            factionId: 'ally',
+            creatureIds: [attacker.id],
+          },
+          {
+            factionId: 'enemy',
+            creatureIds: [enemy.id],
+          }
+        ],
+        battleFieldMatrix: matrix,
+        attackerCreatureId: attacker.id,
+      }
+
+      it('can not attack the enemy', function() {
+        const newContext = invokeNormalAttack(context)
+        const newEnemy = findCreatureByIdOrError(newContext.creatures, enemy.id)
+        assert.strictEqual(newEnemy.lifePoint, 1)
+      })
     })
   })
 })
